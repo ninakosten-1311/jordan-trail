fe01edf0360bcb243cb136dfa22ebea683233b99
ninakosten-1311 <ninakosten@gmail.com>
2026-09-07 10:15:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1217,8 +1217,7 @@ for (const run of stravaRuns) {
     totalDistance = totalDistance + run.distanceKm;
 }
 
-// TEMPORARY TESTING 
-totalDistance =80;
+// TEMPORARY TESTING totalDistance =80;
 
 updateProgressDisplay();
 
